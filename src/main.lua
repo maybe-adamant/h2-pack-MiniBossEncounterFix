3a160fda8a2f075252b74b3be228f245328066ec
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:09 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Fixes Miniboss with top screen health bars not properly progressing biome depth.",
     default  = true,
     dataMutation = true,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
